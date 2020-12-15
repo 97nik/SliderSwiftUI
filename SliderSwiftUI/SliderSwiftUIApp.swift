c6d77024c9f3e09adc8140a8ea7f56112285e485
Nikita <97m.nik@gmail.com>
2020-12-15 16:24:05 +0300

    Initial Commit

---
+++ SliderSwiftUI/SliderSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  SliderSwiftUIApp.swift
+//  SliderSwiftUI
+//
+//  Created by Никита on 15.12.2020.
+//
+
+import SwiftUI
+
+@main
+struct SliderSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
